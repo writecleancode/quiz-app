d4b49487770bf76be1a1fd50a072eb98e3e2c5e2
Rafal <rafek131@wp.pl>
2024-02-19 11:00:20 +0100

    'Coutries of Europe quiz' updates: timer is now working, 'hide map' button is working, styles improvements

--- src/views/Quizes/CountriesOfEurope.tsx
+++ src/views/Quizes/CountriesOfEurope.tsx
@@ -4,8 +4,32 @@ import { QuizWrapper } from 'src/components/templates/QuizWrapper/QuizWrapper';
 import { QuizHeader } from 'src/components/molecules/QuizHeader/QuizHeader';
 import styles from './CountriesOfEurope.module.scss';
 
+const initialTime = 120; // initial time in seconds
+
+const timeLeftText = (secondsTotal = initialTime) => {
+	const minutes = Math.floor(secondsTotal / 60);
+	const seconds = secondsTotal % 60;
+
+	return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
 export const CountriesOfEurope = () => {
 	const [isQuizStarted, setQuizState] = useState(false);
+	const [inputValue, setInputValue] = useState('');
+	const [timeLeft, setTimeLeft] = useState(timeLeftText);
+	const [isMapDisplayed, setMapDisplay] = useState(true);
+
+	const handleStartQuiz = () => {
+		setQuizState(true);
+		let secondsTotal = initialTime;
+
+		const interval = setInterval(() => {
+			secondsTotal--;
+			setTimeLeft(timeLeftText(secondsTotal));
+
+			if (secondsTotal <= 0) clearInterval(interval);
+		}, 1000);
+	};
 
 	return (
 		<QuizWrapper>
@@ -13,48 +37,55 @@ export const CountriesOfEurope = () => {
 				title={quizzes[0].title}
 				description='wymień jak najwięcej krajów leżących na terenie Europy w ciągu 2 minut'
 			/>
-			<div className={styles.controlsWrapper}>
-				{isQuizStarted ? (
-					<div className={styles.formWrapper}>
-						<div className={styles.formField}>
-							<label className={styles.formField__label} htmlFor='country'>
-								Wpisz nazwę państwa:
-							</label>
-							<input className={styles.formField__input} type='text' id='country' name='country' />
+			<div className={styles.wrapper}>
+				<div className={styles.controlsWrapper}>
+					{isQuizStarted ? (
+						<div className={styles.formWrapper}>
+							<div className={styles.formField}>
+								<label className={styles.formField__label} htmlFor='country'>
+									Wpisz nazwę państwa:
+								</label>
+								<input className={styles.formField__input} value={inputValue} onChange={(e) => setInputValue(e.target.value)} type='text' id='country' name='country' />
+							</div>
+							<div className={styles.formInfo}>
+								<p className={styles.formInfo__progress}>13 / 46</p>
+								<p className={styles.formInfo__timeLeft}>{timeLeft}</p>
+							</div>
+							<div className={styles.formButtons}>
+								<button className={styles.formButtons__addMoreTimeButton} type='button'>
+									Chcę jeszcze 2 minuty!
+								</button>
+								<button className={styles.formButtons__giveUpButton} type='button'>
+									Poddaję się 😕
+								</button>
+							</div>
 						</div>
-						<div className={styles.formInfo}>
-							<p className={styles.formInfo__progress}>13 / 46</p>
-							<p className={styles.formInfo__timeLeft}>1:37</p>
-						</div>
-						<div className={styles.formButtons}>
-							<button className={styles.formButtons__addMoreTimeButton} type='button'>
-								Chcę jeszcze 2 minuty!
-							</button>
-							<button className={styles.formButtons__giveUpButton} type='button'>
-								Poddaję się 😕
-							</button>
-						</div>
-					</div>
-				) : (
-					<button className={styles.startButton} onClick={() => setQuizState(true)} type='button'>
-						Rozpocznij quiz
-					</button>
-				)}
-			</div>
-			<div className={styles.helpersWrapper}>
-				<div>
-					<button className={styles.toggleMapDisplayButton} type='button'>
-						<img src='/src/assets/icons/map.svg' alt='' />
-						<span>Pokaż mapę pomocniczą</span>
-					</button>
-					<img className={styles.mapImg} src='/src/assets/img/europe_map.jpg' alt='mapa Europy' />
+					) : (
+						<button className={styles.startButton} onClick={handleStartQuiz} type='button'>
+							Rozpocznij quiz
+						</button>
+					)}
 				</div>
-				<div className={styles.resultsWrapper}>
-					<div className={styles.resultsWrapper__column}>
-						<p className={styles.result}>Albania</p>
+				<div className={styles.helpersWrapper}>
+					<div>
+						<button
+							className={styles.toggleMapDisplayButton}
+							onClick={() => setMapDisplay(prevState => !prevState)}
+							type='button'>
+							<img src='/src/assets/icons/map.svg' alt='' />
+							<span>Pokaż mapę pomocniczą</span>
+						</button>
+						{isMapDisplayed ? (
+							<img className={styles.mapImg} src='/src/assets/img/europe_map.jpg' alt='mapa Europy' />
+						) : null}
 					</div>
-					<div className={styles.resultsWrapper__column}>
-						<p className={styles.result}>Bośnia i Hercegowina</p>
+					<div className={styles.resultsWrapper}>
+						<div className={styles.resultsWrapper__column}>
+							<p className={styles.result}>Albania</p>
+						</div>
+						<div className={styles.resultsWrapper__column}>
+							<p className={styles.result}>Bośnia i Hercegowina</p>
+						</div>
 					</div>
 				</div>
 			</div>
